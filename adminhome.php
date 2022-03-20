690ae131f07e41f6d69f7f1ce0edbbee6b4f8ffa
Balazs Ferencz <64424862+Pici1985@users.noreply.github.com>
2022-03-20 14:31:06 +0000

    cleanup

--- adminhome.php
+++ adminhome.php
@@ -9,23 +9,6 @@ if(!isset($_SESSION["username"]))
 
 ?>
 
-
-<!-- <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <a href="index.php">Home</a>
-        <h1>Admin homepage</h1>
-        <h2>Welcome: <?php echo $_SESSION["username"] ?> </h2>
-        <a href="logout.php">Logout</a>
-    </body>
-</html> -->
-
 <?php
 
 // import statement
